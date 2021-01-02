b97bf4decd0315e0dde7fbb31c4f0a1b530a6794
grillazz <the@grillazz.com>
2021-01-02 22:14:42 +0100

    black

--- twofa_for_drf/wsgi.py
+++ twofa_for_drf/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twofa_for_drf.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twofa_for_drf.settings")
 
 application = get_wsgi_application()
